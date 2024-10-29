da5e3f5f0955f7b53c4a12bfe0eda00b2bf773da
DanChass <91178452+DanChass@users.noreply.github.com>
2024-10-30 01:18:38 +1000

    Add files via upload

---
+++ LeaderBoard.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Otladka
+{
+    [Serializable]
+    internal class LeaderBoard
+    {
+
+        private List<User> users;
+        private string filePath;
+
+        public LeaderBoard(string filePath)
+        {
+            this.filePath = filePath;
+            LoadUsers();
+        }
+        public bool AuthUser(string login, string password)
+        {
+            //Поиск пользователя по логину и проверка пароля
+            var user = users.FirstOrDefault(u => u.Login == login && u.Password == password);
+            return (user != null);
+        }
+
+        public bool RegisterUser(string login, string password)
+        {
+            // Проверяем, существует ли пользователь с таким логином
+            if (users.Any(u => u.Login == login))
+            {
+                Console.WriteLine("Пользователь с таким логином уже существует!");
+                return false;
+            }
+
+            //Создаю нового пользователя и добавляю его в список пользователей (после сразу сериализация)
+            users.Add(new User { Login = login, Password = password });
+            SaveUsers();
+            return true;
+        }
+
+        //Добавление результата для конкретного пользователя
+        public void AddScore(string login, int score)
+        {
+            var user = users.FirstOrDefault(u => u.Login == login);
+            if (user != null)
+            {
+                user.Scores.Add(score);
+                SaveUsers();
+            }
+            else
+            {
+                Console.WriteLine("Пользователь не найден!");
+            }
+        }
+
+        public int ShowPlace(string login, int score)
+        {
+            var user = users.FirstOrDefault(u => u.Login == login);
+            if (user == null || user.Scores.Count == 0)
+            {
+                Console.WriteLine("Результаты пользователя не найдены.");
+                return 0;
+            }
+
+            //Собираю все результаты в сортированный по возрастанию список
+            var allScores = users.SelectMany(u => u.Scores).OrderBy(u => u).ToList();
+
+            int place = allScores.IndexOf(score) + 1;
+            return 100 * place / allScores.Count;
+        }
+
+        //Десериализация данных из файла
+        private void LoadUsers()
+        {
+            if (File.Exists(filePath))
+            {
+                var json = File.ReadAllText(filePath);
+                users = JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
+            }
+            else
+            {
+                users = new List<User>();
+            }
+        }
+
+        //Сериализация данных в JSON и запись в файл
+        private void SaveUsers()
+        {
+            var json = JsonSerializer.Serialize(users, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,147 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Otladka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "leaderboard.json";
+            LeaderBoard leaderBoard = new LeaderBoard(filePath);
+            string login = null;
+            string password = null;
+            bool auth = false;
+
+            while (!auth)
+            {
+                Console.WriteLine("\nВыберите действие: 1 - Регистрация, 2 - Вход");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Console.Write("Введите логин: ");
+                    login = Console.ReadLine();
+                    Console.Write("Введите пароль: ");
+                    password = Console.ReadLine();
+
+                    if (leaderBoard.RegisterUser(login, password))
+                    {
+                        Console.WriteLine("Регистрация прошла успешно.");
+                        auth = true;
+                    }
+                }
+                else if (choice == 2)
+                {
+                    Console.Write("Введите логин: ");
+                    login = Console.ReadLine();
+                    Console.Write("Введите пароль: ");
+                    password = Console.ReadLine();
+
+                    if (leaderBoard.AuthUser(login, password))
+                    {
+                        Console.WriteLine("Вход выполнен.");
+                        auth = true;
+                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный логин или пароль.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный выбор.");
+                }
+            }
+            
+            int rightPlace = 0;
+            int guessed = 0;
+            int res = 0;
+            Random random = new Random();
+
+            //массив цифр от 0 до 9
+            int[] digits = Enumerable.Range(0, 10).ToArray();
+
+            //рандомно перемешиваю массив
+            digits = digits.OrderBy(x => random.Next()).ToArray();
+
+            //Проверяю, чтобы первая цифра не была 0
+            if (digits[0] == 0)
+            {
+                //Меняю местами ноль с первой ненулевой цифрой
+                for (int i = 1; i < digits.Length; i++)
+                {
+                    if (digits[i] != 0)
+                    {
+                        int temp = digits[0];
+                        digits[0] = digits[i];
+                        digits[i] = temp;
+                        break;
+                    }
+                }
+            }
+            //первые 4 цифры - загаданное число
+            string number = string.Join("", digits.Take(4));
+            Console.WriteLine("\nЗагадано 4-значное число с неповторяющимися цифрами.");
+
+            //Цикл для отгадывания числа
+            while (true)
+            {
+                rightPlace = 0;
+                guessed = 0;
+                Console.Write("Попробуйте угадать 4-значное число: ");
+                string check = Console.ReadLine();
+
+                //проверка длины введенной пользователем строки
+                if (check.Length != 4)
+                {
+                    Console.WriteLine("В числе должно быть 4 знака!\n");
+                    continue;
+                }
+
+                //Проверка введеной пользователем строки на целое число
+                if (!int.TryParse(check, out int parsed))
+                {
+                    Console.WriteLine("Запись числа должна состоять только из цифр!\n");
+                    continue;
+                }
+
+                //засчитываю попытку
+                res++;
+
+                //перебираю символы загаданного числа, сравнивая с введенным пользователем
+                foreach (char c in number)
+                {
+                    foreach (char chr in check)
+                    {
+                        if (chr.Equals(c))
+                        {
+                            guessed++;
+                            break;
+                        }
+                    }
+                }
+                
+                //проверка верного расположения символов
+                for (int i = 0; i < 4; i++)
+                {
+                    if (number[i].Equals(check[i]))
+                        rightPlace++;
+                }
+                
+                //если число угадано
+                if (rightPlace == 4)
+                {
+                    leaderBoard.AddScore(login, res);
+                    int percent = leaderBoard.ShowPlace(login, res);
+                    Console.WriteLine("Вы угадали число за " + res + " попыток(ки)! Этот результат входит в " + percent + "% лучших!");
+                    break;
+                }
+                else
+                    Console.WriteLine(guessed + " угадано, " + rightPlace + " на верном месте! Пробуйте еще.\n");
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
